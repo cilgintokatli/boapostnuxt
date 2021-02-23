6e34608b88815111bdc73b2d0ad6c0d2c8dc6262
Okan Sahin <okansahinfed@gmail.com>
2021-02-23 15:21:15 +0300

    update icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' }
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' }
     ]
   },
 
