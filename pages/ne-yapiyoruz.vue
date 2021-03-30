4eabdf810e507404cf24dcaf117e6bf0705176fe
Okan Sahin <okansahinfed@gmail.com>
2021-03-30 19:21:24 +0300

    spelling corrected

--- pages/ne-yapiyoruz.vue
+++ pages/ne-yapiyoruz.vue
@@ -7,37 +7,37 @@
       <div class="text-left mb-20"> 
         <h3 class="text-yesil text-5xl">Edit</h3>
             <p>
-            Kurgu; Çekilen görüntülerin senaryoya uygun şekilde, içinde çeşitli değişkenler barındıran matematiksel bir örgüyle kurulması/dizilmesi işidir.
+            Kurgu; çekilen görüntülerin senaryoya uygun şekilde, içinde çeşitli değişkenler barındıran matematiksel bir örgüyle kurulması/dizilmesi işidir.
 
-            boabost; Profesyonel ekip ve yeni nesil ekipmanlarıyla drama, belgesel, reklam, tanıtım ve dijital içeriklerin kurgusunu yapar.
+            Boabost; profesyonel ekip ve yeni nesil ekipmanlarıyla drama, belgesel, reklam, tanıtım ve dijital içeriklerin kurgusunu yapar.
             </p>
 
         <h3 class="text-yesil text-5xl">Color</h3>
             <p>
             Görsel dünyada atmosfer yaratmanın en önemli etkenlerinden biri renktir. Sinema ve video endüstrisinde içeriğin renk atmosferinin yaratılması temel dallardan biridir.
 
-            boabost; Renk düzenlemesi için özel tasarlanmış stüdyosunda, profesyonel ekip ve ekipmanlarıyla, endüstri ihtiyaçlarına, içeriğin türüne ve içerik üreticisinin taleplerine göre, içeriklerin renk düzenlemesini yapar.
+            Boabost; renk düzenlemesi için özel tasarlanmış stüdyosunda, profesyonel ekip ve ekipmanlarıyla, endüstri ihtiyaçlarına, içeriğin türüne ve içerik üreticisinin taleplerine göre, içeriklerin renk düzenlemesini yapar.
             </p>
 
         <h3 class="text-yesil text-5xl">Sound</h3>
             <p> 
-            Sinema ve video içeriklerindeki ses düzenlemesi, görüntü kadar önemli bir unsurdur. Sesin doğru kullanımı ve doğru tasarlanması üretilen eserin kalitesini yukarı taşıdığı gibi izleyiciye yaşatılmak istenen duygunun da güçlenmesini sağlar.
+            Sinema ve video içeriklerindeki ses düzenlemesi, görüntü kadar önemli bir unsurdur. Sesin doğru kullanımı ve doğru tasarlanması, üretilen eserin kalitesini yukarı taşıdığı gibi izleyiciye yaşatılmak istenen duygunun da güçlenmesini sağlar.
 
             Boapost; tüm video içeriklerin ses tasarımını, seslerin foley ile güçlendirilmesini, ses ve müzik miksajını yapar.
             </p>
 
         <h3 class="text-yesil text-5xl">Graphic</h3>
             <p>
-            Sinema ve video endüstrisinde grafik, vfx, 2D/3D animasyon işlemleri vageçilmez bir alandır. İçeriğin izleyiciye aktarılmak istenen biçimine kavuşmasına yardımcı olduğu gibi üreticinin de yaratıcılığının sınırlarını zorlamasına yardımcı olur. Bu alanda iyi sonuçlar elde etmek, hem profesyonel ve deneyimli bir ekip hem de gelişmiş teknolojik alt yapı ve ekipmanlara sahip olmaya bağlıdır.
+            Sinema ve video endüstrisinde grafik, vfx, 2D/3D animasyon işlemleri vageçilmez bir alandır. İçeriğin, izleyiciye aktarılmak istenen biçimine kavuşmasına yardımcı olduğu gibi üreticinin de yaratıcılığının sınırlarını zorlamasına yardımcı olur. Bu alanda iyi sonuçlar elde etmek, hem profesyonel ve deneyimli bir ekip hem de gelişmiş teknolojik alt yapı ve ekipmanlara sahip olmaya bağlıdır.
 
-            Boapost; Sinema ve video içeriklerin ihtiyaç duyduğu grafik desteğini, elindeki gelişmiş ekipmanla, hızlı ve sonuç odaklı çözümlerle üreticiye sunar.
+            Boapost; sinema ve video içeriklerin ihtiyaç duyduğu grafik desteğini, elindeki gelişmiş ekipmanla, hızlı ve sonuç odaklı çözümlerle üreticiye sunar.
             </p>
 
         <h3 class="text-yesil text-5xl">Kurgu ve Color Stüdyoları</h3>
             <p>
             Post prodüksiyon süreci yoğun mesai ve bunun yanında yaratıcılık isteyen bir süreçtir. Bu süreçte çalışılan ortamın konforlu olması ve stüdyonun tüm teknolojik ihtiyaçları karşılaması önemli bir gerekliliktir.
 
-            Boapost; Sinema ve video üreticilerine konforlu ve içeriğin gerektirdiği tüm teknolojik imkanı sağlayan kurgu ve color stüdyosu kiralama hizmeti verir.
+            Boapost; sinema ve video üreticilerine konforlu ve içeriğin gerektirdiği tüm teknolojik imkanı sağlayan kurgu ve color stüdyosu kiralama hizmeti verir.
             </p>
         </div>
     </div>
