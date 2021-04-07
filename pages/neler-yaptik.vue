d75f195560fb6439448670b4922868b242f5893d
Okan Sahin <okansahinfed@gmail.com>
2021-04-07 10:15:06 +0300

    fixing children pages

--- pages/neler-yaptik.vue
+++ pages/neler-yaptik.vue
@@ -3,13 +3,14 @@
       <h1 class="text-6xl text-yesil text-center font-bold">
         neler yaptık?
       </h1>
-      <div class="vimeos">
+      <div class="vimeos gap-8">
           <div v-for="(video, index) in videos" :key="index"> 
-              <span> <img @click="show(video)" :src="video.img"> </span>
-            <span>{{ video.name }}</span>
-            
+            <span> <nuxt-link :to="`${video.id}`" ><img :src="video.img" class="mb-3"></nuxt-link>  </span>
+            <span class="font-bold mt-4">{{ video.name }}</span>
+             
           </div>
       </div>
+       <nuxt-child :key="$route.params.id" ></nuxt-child>
       <modal name="my-first-modal" :adaptive="true">
         <div v-html="selectedItem.embed"></div>
     </modal>
@@ -39,25 +40,18 @@ methods: {
     },
   async mounted() {
     try {
+      console.time('timer')
       this.$axios.setToken('5e2f0b63d81b85ee46efc12cabd07fed', 'Bearer')
       const res = await this.$axios.get(`https://api.vimeo.com/me/videos`)
       const results = res.data.data
       console.log(results)
-
-      const letters = { "İ":"i", "I":"i", "ı":"i", "Ş":"s", "ş":"s", "Ğ":"g", "ğ":"g", "Ü":"u", "ü":"u", "Ö":"o", "ö":"o", "Ç":"c", "ç":"c" };
+      console.timeEnd('timer')
 
       this.videos = results.map(data => ({
         name: data.name,
         img: data.pictures.sizes[4].link,
-        link: data.link.replace('https://vimeo.com/',''),
+        id: data.link.replace('https://vimeo.com/',''),
         embed: data.embed.html,
-        slug: data.name.toString().toLowerCase()
-        .replace(/[İIıŞşĞğÜüÇçÖö]/g, function(letter){ return letters[letter]; })
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
       }))
       
     }
@@ -83,9 +77,9 @@ methods: {
 <style>
 .vimeos{
   display: grid;
-    grid-gap: 40px;
+    
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-auto-rows: 300px;
+    grid-auto-rows: min-content;;
     margin-top: 40px;
 }
 
---
+++ pages/neler-yaptik/_id.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <div class="inset-0 fixed z-30" @click.self="console.log('clicked mı vallah')" >
+      <div  class="grid place-items-center min-h-screen pt-4 px-4 pb-20 text-center block sm:p-0">
+        <div  class="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div class="absolute inset-0 bg-black opacity-20" style="opacity:0.2"></div>
+        </div>
+        
+        <div class="bg-black rounded-3xl z-50 shadow-xl relative">
+          <nuxt-link to="/neler-yaptik/"
+          class="modal-close cursor-pointer z-50 absolute -right-2" style="right: -3rem">
+            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 18 18">
+              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+            </svg>
+          </nuxt-link>
+            <client-only>
+                <vimeo-player ref="player" :video-id="videoID"/>
+            </client-only>	
+        </div>
+      </div>
+    </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+    'vimeoID'
+  ],
+    data() {
+        return {
+            videoID: this.$route.params.id
+        }
+    }
+}
+</script>
